32a67ce089f99c77ec0ba4086618b15bd8d7a1ea
yuanyue970918 <543588053@qq.com>
2017-11-20 21:49:51 +0800

    the second

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -27,6 +27,7 @@ class Adafruit_GFX : public Print {
   virtual void writeFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
   virtual void writeFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
   virtual void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
+  virtual void drawEllipse(int16_t x0, int16_t y0, int16_t a, int16_t b, uint16_t color);
   virtual void endWrite(void);
 
   // CONTROL API
